f141707079957c6d24dcbd924e42644cfbead8c0
seecherguber <joelucascool@gmail.com>
2023-10-15 02:03:58 +1300

    seperated cell generation and neighbor counting into two functions

--- src/gamemain.java
+++ src/gamemain.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
+
 public class GameMain {
     static int GRIDSIZE = 10;
     // This is the main class for the game conway's game of life
@@ -8,31 +8,40 @@ public class GameMain {
     //rule 3: any live cell with more than three live neighbors dies, as if by overpopulation
     //rule 4: any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //printing grid
-        GameMain game = new GameMain();
-        int [][] gameGrid = new int[GRIDSIZE][GRIDSIZE];
-        game.gridPrint(gameGrid);
+        int[][] gameGrid = new int[GRIDSIZE][GRIDSIZE];
+        GameMain.gridPrint(gameGrid);
         System.out.println("how many cells would you like to start with?");
         Scanner input = new Scanner(System.in);
         int numCells = input.nextInt();
-        for(int i = 0; i < numCells; i++){
-            System.out.println("enter the x coordinate of cell " + (i+1));
-            int x = input.nextInt();
-            System.out.println("enter the y coordinate of cell " + (i+1));
-            int y = input.nextInt();
-            gameGrid[x][y] = 1;
-            game.gridPrint(gameGrid);
+        for (int i = 0; i < numCells; i++) {
+            System.out.println("enter the x coordinate of cell " + (i + 1));
+            int x = input.nextInt() - 1;
+            System.out.println("enter the y coordinate of cell " + (i + 1));
+            int y = input.nextInt() - 1;
+            gameGrid[y][x] = 1;
+            GameMain.gridPrint(gameGrid);
         }
         System.out.println("how many generations would you like to simulate?");
         int numGens = input.nextInt();
-        for(int i = 0; i < numGens; i++){
-            game.neighborCheck(gameGrid);
-            game.gridPrint(gameGrid);
-            try{TimeUnit.SECONDS.sleep(2);} catch(InterruptedException e){};
+        for (int i = 0; i < numGens; i++) {
+            System.out.println("generation " + (i + 1));
+            int neighborCount = GameMain.neighborCheck(gameGrid);
+            cellGeneration(neighborCount, gameGrid);
+            GameMain.gridPrint(gameGrid);
+
+            int secondsToSleep = 1;
+            try {
+                Thread.sleep(secondsToSleep * 1000);
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+            }
+
         }
-}
+    }
+
     static void gridPrint(int[][] gameGrid) { //prints grid
         for (int i = 0; i < GRIDSIZE; i++) {
             for (int j = 0; j < GRIDSIZE; j++) {
@@ -41,50 +50,57 @@ public class GameMain {
             System.out.println();
         }
     }
+
     //checking neighbours
-    static void neighborCheck(int[][] gameGrid){
-    for(int i = 0; i < GRIDSIZE; i++){
-        for(int j = 0; j < GRIDSIZE; j++){
-            int neighbors = 0;
-            int cell = gameGrid[i][j];
-            if(i > 0 && j > 0 && gameGrid[i-1][j-1] == 1){
-                neighbors++;
+    static int neighborCheck(int[][] gameGrid) {
+        int neighbors = 0;
+        for (int i = 0; i < gameGrid.length; i++) {
+            for (int j = 0; j < gameGrid.length; j++) {
+                int cell = gameGrid[i][j];
+                if (i > 0 && j > 0 && gameGrid[i - 1][j - 1] == 1) {
+                    neighbors++;
+                }
+                if (i > 0 && gameGrid[i - 1][j] == 1) {
+                    neighbors++;
+                }
+                if (i > 0 && j < gameGrid.length - 1 && gameGrid[i - 1][j + 1] == 1) {
+                    neighbors++;
+                }
+                if (j > 0 && gameGrid[i][j - 1] == 1) {
+                    neighbors++;
+                }
+                if (j < gameGrid.length - 1 && gameGrid[i][j + 1] == 1) {
+                    neighbors++;
+                }
+                if (i < gameGrid.length - 1 && j > 0 && gameGrid[i + 1][j - 1] == 1) {
+                    neighbors++;
+                }
+                if (i < gameGrid.length - 1 && gameGrid[i + 1][j] == 1) {
+                    neighbors++;
+                }
+                if (i < gameGrid.length - 1 && j < gameGrid.length - 1 && gameGrid[i + 1][j + 1] == 1) {
+                    neighbors++;
+                }
+
             }
-            if(i > 0 && gameGrid[i-1][j] == 1){
-                neighbors++;
-            }
-             if(i > 0 && gameGrid[i-1][j+1] == 1){
-                neighbors++;
-            }
-             if(i > 0 && gameGrid[i][j-1] == 1){
-                neighbors++;
-            }
-            if(i > 0 && gameGrid[i][j+1] == 1){
-                neighbors++;
-            }
-            if(i > 0 && gameGrid[i+1][j-1] == 1){
-                neighbors++;
-            }
-            if(i > 0 && gameGrid[i+1][j] == 1){
-                neighbors++;
-            }
-            if(i > 0 && gameGrid[i+1][j+1] == 1){
-                neighbors++;
-            }
-            if(neighbors < 2){
-                gameGrid[i][j] = 0;
-            }
-            if(neighbors == 2 && cell ==1 || neighbors == 3 && cell == 1){
-                gameGrid[i][j] = 1;
-            }
-            if(neighbors > 3){
-                gameGrid[i][j] = 0;
-            }
-            if(neighbors == 3 && cell == 0){
-                gameGrid[i][j] = 1;
+        }
+        return neighbors;
+    }
+
+    static void cellGeneration(int neighborCount, int[][] gameGrid) {
+        for (int i = 0; i < gameGrid.length; i++) {
+            for (int j = 0; j < gameGrid.length; j++) {
+                int cell = gameGrid[i][j];
+                if (cell == 1 && neighborCount < 2) {
+                    gameGrid[i][j] = 0;
+                }
+                if (cell == 1 && neighborCount > 3) {
+                    gameGrid[i][j] = 0;
+                }
+                if (cell == 0 && neighborCount == 3) {
+                    gameGrid[i][j] = 1;
+                }
             }
         }
     }
-
-}
 }
